bd5ff581f14cfc8983f572a68cd6fabeed309861
MilenIvanovv <ivanovvmilen@gmail.com>
2020-01-26 21:02:48 +0200

    Child process service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { SubmissionsModule } from './submissions/submissions.module';
 import { CoreModule } from './core/core.module';
 import { FsService } from './core/services/fs/fs.service';
+import { ChildProcessService } from './core/services/child-process/child-process.service';
 
 @Module({
   imports: [SubmissionsModule, CoreModule],
   controllers: [AppController],
-  providers: [AppService, FsService],
+  providers: [AppService, FsService, ChildProcessService],
 })
 export class AppModule {}
---
+++ server/src/core/services/child-process.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import * as cp from 'child_process';
+
+@Injectable()
+export class ChildProcessService {
+  async execFile(pathToFile: string): Promise<any> {
+    const parameters = [];
+    const options = {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+    };
+
+    const child = cp.fork(pathToFile, parameters, options as any);
+    child.on('message', message => {
+      console.log('message from child:', message);
+      child.send('Hi');
+    });
+  }
+
+  async combineFiles(file1Path, file2Path, outputFilePath) {
+    await new Promise((res, rej) => {
+      cp.exec(`type ${file1Path} ${file2Path} > ${outputFilePath}`, (err) => {
+        if (err) {
+          throw err;
+        }
+        res();
+      });
+    });
+  }
+}
